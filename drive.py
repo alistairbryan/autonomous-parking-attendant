11bd7cd5d4e263a6ffe3a3524b83cfa73648cc10
ckilzi <32025834+ckilzi@users.noreply.github.com>
2019-11-26 19:24:07 -0800

    Add files via upload

--- drive.py
+++ drive.py
@@ -38,34 +38,36 @@ def getState(self, data):
 	y = imageUn.shape[1]
 	sec = x/10
 	#CHECK FOR CROSSWALK RED
-	crossPx = cv.countNonZero(cv.inRange(cv.cvtColor(imageUn,cv.COLOR_BGR2HSV), self.lower_red,self.upper_red ))
-	if (crossPx > 20):
+	crossPx = cv.countNonZero(cv.inRange(cv.cvtColor(im,cv.COLOR_BGR2HSV), self.lower_red,self.upper_red ))
+	if (crossPx > 10):
 		if self.notCrossWalk == 0: #if we have seen crosswalk
-			if self.crossWalk == 0:  
+			if self.crossWalk == 1:  
 				self.crossWalk = 0
 				self.notCrossWalk = 1
+			else:
+				self.crossWalk = 1
 	else:
 		if self.notCrossWalk == 1:
 			self.notCrossWalk = 0
-	#SET STATES
+	#if self.crossWalk == 1:
+		#SET STATES
 	for i in range(0,9):
 		section = imageUn[0:y, sec*i:sec*(i + 1)]
 
 		#ROAD
-		roadPx = cv.countNonZero(cv.inRange(cv.cvtColor(section,cv.COLOR_BGR2HSV), self.lower_road,self.upper_road ))		
+		roadPx = cv.countNonZero(cv.inRange(cv.cvtColor(section,cv.COLOR_BGR2HSV), self.lower_road,self.upper_road ))
 		#EVERYTHING THAT ISN'T ROAD
 		carPx = cv.countNonZero(cv.inRange(cv.cvtColor(section,cv.COLOR_BGR2HSV), self.lower_car,self.upper_car ))
 		grassPx = cv.countNonZero(cv.inRange(cv.cvtColor(section,cv.COLOR_BGR2HSV), self.lower_grass,self.upper_grass ))
 		whitePx = cv.countNonZero(cv.inRange(cv.cvtColor(section,cv.COLOR_BGR2HSV), self.lower_white,self.upper_white ))*self.crossWalk
-		notRoadPx = 0.8*float(carPx + grassPx + whitePx)
-
+		notRoadPx = 0.8*float(carPx + grassPx + whitePx*(self.crossWalk))
 		if carPx > 10: #CAR IN COLUMN: 2
 			state[i] = 2
 		elif roadPx < notRoadPx: #NOT ROAD: 0
 			state[i] = 0
 		else: #ROAD : 1
 			state[i] = 1
-	print(state)
+
 	return state
 
 
@@ -92,7 +94,8 @@ class Drive(gazebo_env.GazeboEnv):
 		self.upper_white = np.array([200, 100 ,255])
 		self.lower_road = np.array([0,0, 60])
 		self.upper_road = np.array([0, 0, 90])
-
+		self.i = 0
+		self.startingTurn = [0,0, 0,0,1,1,1,1,1,1]
 
 		self.action_space = spaces.Discrete(3)  # F,L,R
 		self.actions =  range(self.action_space.n)
@@ -106,28 +109,33 @@ class Drive(gazebo_env.GazeboEnv):
 
 
 	def callback(self, data):
-		#state = ''.join(map(str, [0,0,1,1,1,1,1,1,0,0]))
-		#print(self.q.get(state, 1), 0.0)
-		#the state determined from camera 
-		stateVal = getState(self, data)
-		state = ''.join(map(str, stateVal))
-		#for i in range(0, 2):
-			#getQ(self, state, a)
-		q = [getQ(self, state, a) for a in self.actions]
-		maxQ = max(q)
+				
+		if self.i > len(self.startingTurn)-1:
+			#the sate determined from camera 
+			stateVal = getState(self, data)
+			state = ''.join(map(str, stateVal))
+			#for i in range(0, 2):
+				#getQ(self, state, a)
+			q = [getQ(self, state, a) for a in self.actions]
+			maxQ = max(q)
 
-		#If more than one Q
-		count = q.count(maxQ)
-		if count > 1:
-			best = [i for i in range(len(self.actions)) if q[i] == maxQ]
-			i = random.choice(best)
+			#If more than one Q
+			count = q.count(maxQ)
+			if count > 1:
+				best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+				i = random.choice(best)
+			else:
+				i = q.index(maxQ)
+			action = self.actions[i]
+
+
 		else:
-			i = q.index(maxQ)
-		action = self.actions[i]
+			action = self.startingTurn[self.i]
+			print(action)
+			self.i +=1
 
 		#WRITE COMMAND
 		vel_cmd = Twist()
-
 		if action == 0:  # FORWARD
 			vel_cmd.linear.x = 1
 			vel_cmd.angular.z = 0.0
@@ -137,7 +145,6 @@ class Drive(gazebo_env.GazeboEnv):
 		elif action == 2:  # RIGHT
 			vel_cmd.linear.x = 0.0
 			vel_cmd.angular.z = -0.3
-		print(vel_cmd)		
 		self.vel_pub.publish(vel_cmd)		
 
 
