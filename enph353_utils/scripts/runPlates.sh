1deea50bb4204aa83fcb428de5a909500d085bc7
ckilzi <32025834+ckilzi@users.noreply.github.com>
2019-11-28 19:55:30 -0800

    Add files via upload

---
+++ enph353_utils/scripts/runPlates.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /home/chloe/353_ws/devel/setup.bash
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_IP=127.0.0.1  
+
+python /home/chloe/353_ws/src/2019F_competition_students/enph353/enph353_gazebo/nodes/object_recognition.py &
